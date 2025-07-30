77d5d8e2568dbeba7e8721f56aa860dea8c91d52
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2025-07-31 01:29:41 +0200

    chore: decrease page section spacing

--- src/pages/edit-category/ui/edit-category-page.tsx
+++ src/pages/edit-category/ui/edit-category-page.tsx
@@ -14,7 +14,7 @@ function Page() {
 
   return (
     <main>
-      <div className="mx-auto min-h-dvh w-full max-w-5xl space-y-8 px-4 py-8 lg:space-y-12 lg:py-12">
+      <div className="mx-auto min-h-dvh w-full max-w-5xl space-y-8 px-4 py-8 lg:py-12">
         <div className="flex items-center justify-between gap-x-4">
           <h1 className="font-display text-2xl">Редактирование категории</h1>
           <BackButton />
